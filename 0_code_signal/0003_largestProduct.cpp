14d7332d9a07ab6da78ea3fbf488a3ca09b8f993
sla-ppy <sla-ppy-pm@protonmail.com>
2022-04-09 23:04:06 +0200

    largestproduct, shapearea

---
+++ 0_code_signal/0003_largestProduct.cpp
@@ -0,0 +1,15 @@
+largestProduct
+
+    std::vector<int> inputArray = {-23, 4, -3, 8, -12};
+
+    // we need to set the base value of our integer to the smallest possible integer value
+    // so if we have a negative number 
+    int largestProduct{std::numeric_limits<int>::min()};
+    for(int i = 0; i < inputArray.size() -1; i++) {
+        int currentProduct = (inputArray[i] * inputArray[i+1]);
+        if(largestProduct < currentProduct) {
+            largestProduct = currentProduct;
+        }
+    }
+    std::cout << largestProduct;
+    return largestProduct;
