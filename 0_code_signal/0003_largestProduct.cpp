d30216496e9fbbc836a3f44aa4aa1e360d3ba497
sla-ppy <sla-ppy-pm@protonmail.com>
2022-07-19 18:07:23 +0200

    fix styling

--- 0_code_signal/0003_largestProduct.cpp
+++ 0_code_signal/0003_largestProduct.cpp
@@ -1,15 +1,13 @@
-largestProduct
+std::vector<int> inputArray = {-23, 4, -3, 8, -12};
 
-    std::vector<int> inputArray = {-23, 4, -3, 8, -12};
-
-    // we need to set the base value of our integer to the smallest possible integer value
-    // so if we have a negative number 
-    int largestProduct{std::numeric_limits<int>::min()};
-    for(int i = 0; i < inputArray.size() -1; i++) {
-        int currentProduct = (inputArray[i] * inputArray[i+1]);
+// we need to set the base value of our integer to the smallest possible integer value
+// so if we have a negative number
+int largestProduct{std::numeric_limits<int>::min()};
+for(int i = 0; i < inputArray.size() -1; i++) {
+    int currentProduct = (inputArray[i] * inputArray[i+1]);
         if(largestProduct < currentProduct) {
             largestProduct = currentProduct;
         }
     }
-    std::cout << largestProduct;
-    return largestProduct;
+std::cout << largestProduct;
+return largestProduct;
